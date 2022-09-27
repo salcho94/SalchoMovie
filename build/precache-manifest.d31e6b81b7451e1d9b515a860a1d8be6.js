5bf4c52181d053c1b7fde6baffe22b34ced6468d
salcho-mirincom <salcho@mirincom.co.kr>
2022-09-27 17:26:20 +0900

    test

---
+++ build/precache-manifest.d31e6b81b7451e1d9b515a860a1d8be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4ff0e4043452a55a2ce30d6c4d8621",
+    "url": "/MovieProject/index.html"
+  },
+  {
+    "revision": "473e895329f5eaa85865",
+    "url": "/MovieProject/static/css/main.1273322f.chunk.css"
+  },
+  {
+    "revision": "c54c42b86b7688014caa",
+    "url": "/MovieProject/static/js/2.c76a4dc9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MovieProject/static/js/2.c76a4dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473e895329f5eaa85865",
+    "url": "/MovieProject/static/js/main.091c6637.chunk.js"
+  },
+  {
+    "revision": "2b89abd5ec25b7a90d41",
+    "url": "/MovieProject/static/js/runtime-main.6aa24260.js"
+  }
+]);
